0db2a4d7fdad6c025c8009ca1db0228204da063d
petersabio <petersabio191@gmail.com>
2023-04-10 14:28:39 -0400

    plannedcourses class added to fronted

---
+++ FrontEnd/frontend/src/app/model/planned-course.spec.ts
@@ -0,0 +1,7 @@
+import { PlannedCourse } from './planned-course';
+
+describe('PlannedCourse', () => {
+  it('should create an instance', () => {
+    expect(new PlannedCourse()).toBeTruthy();
+  });
+});
