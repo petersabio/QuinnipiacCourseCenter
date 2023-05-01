18a7771f2d76307ca2647544ac4b6bc0305e274c
petersabio <petersabio191@gmail.com>
2023-05-01 15:13:35 -0400

    Added add/remove function to admin page

--- FrontEnd/frontend/src/app/app-routing.module.ts
+++ FrontEnd/frontend/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { CourseCatalogPageComponent } from './components/course-catalog-page/course-catalog-page.component';
 import { MessagingCenterPageComponent } from './components/messaging-center-page/messaging-center-page.component';
 import { CourseSearchFormComponent } from './components/course-catalog-page/course-search-form/course-search-form.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 const routes: Routes = [
   { path: '', component: LoginPageComponent },
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'progress', component: ProgressPageComponent },
   { path: 'course-catalog', component: CourseCatalogPageComponent },
   { path: 'messaging-center', component: MessagingCenterPageComponent },
+  {path: 'admin', component: AdminPageComponent}
 ];
 
 @NgModule({
